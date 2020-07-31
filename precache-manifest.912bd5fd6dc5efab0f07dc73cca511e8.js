e4f109a75410417b9bc953c89e76957523b7c6e4
TheLevius <reflevius@gmail.com>
2020-07-31 15:51:52 +0300

    Updates

---
+++ precache-manifest.912bd5fd6dc5efab0f07dc73cca511e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62faee89701676dd143af1fb0fcf26da",
+    "url": "/incu-cards/index.html"
+  },
+  {
+    "revision": "78be6eb7701e8dc14dbb",
+    "url": "/incu-cards/static/css/main.719068f8.chunk.css"
+  },
+  {
+    "revision": "009835a4b667d14ab10e",
+    "url": "/incu-cards/static/js/2.cf7dd3ae.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/incu-cards/static/js/2.cf7dd3ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78be6eb7701e8dc14dbb",
+    "url": "/incu-cards/static/js/main.00d9ec65.chunk.js"
+  },
+  {
+    "revision": "6d43ea76218cfa4fbb78",
+    "url": "/incu-cards/static/js/runtime-main.ddcbfd7b.js"
+  }
+]);
