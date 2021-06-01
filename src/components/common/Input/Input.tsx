15fdf00cd621d79f1dc53bca19e96a14f86fda3b
TheLevius <reflevius@gmail.com>
2021-06-01 16:34:52 +0300

    refactor: add FontAwesome and Input component(blueprint)

--- src/components/common/Input/Input.tsx
+++ src/components/common/Input/Input.tsx
@@ -1,8 +1,27 @@
-import React from 'react';
-import styles from './Input.module.css';
-const Input = (props: any) => {
-    let placeHolder: string = 'Введите текст';
-    return <input type='text' placeholder={placeHolder} className={styles.mainInput}/>
+import React, {ChangeEvent} from 'react';
+import styles from './Input.module.scss';
+export type InputPropsType = {
+    type?: string
+    placeholder?: string
+    value?: string | undefined
+    className?: string
+    onKeyPress?: (e: React.KeyboardEvent<HTMLInputElement>) => void
+    onChange?: (e: ChangeEvent<HTMLInputElement>) => void
+    onBlur?:(e: ChangeEvent<HTMLInputElement>) => void
 }
-
-export default Input;
+export const Input: React.FC<InputPropsType> = React.memo(({   placeholder = '',
+                                                        type = 'text',
+                                                        value = '',
+                                                        className,
+                                                        ...props}) => {
+    console.log(`render ${value}`);
+    return <label>
+        <span>{placeholder}</span>
+            <input type={type}
+            placeholder={placeholder}
+            className={`${styles.inputText} ${className && styles[className]}`}
+            value={value}
+            onChange={props.onChange}
+            />
+    </label>
+})
