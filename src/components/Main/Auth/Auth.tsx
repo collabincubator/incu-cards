d721978831e40b7d2af25c8e0839c4d055725248
valtikamore <77681004+valtikamore@users.noreply.github.com>
2021-06-05 16:08:29 +0300

    feat: implement error handle and fix authReducer / loading

--- src/components/Main/Auth/Auth.tsx
+++ src/components/Main/Auth/Auth.tsx
@@ -1,38 +1,39 @@
 import React, {useEffect} from 'react';
 import styles from '../Auth/Auth.module.scss';
-import {Switch, Route} from 'react-router-dom';
+import {Switch, Route, Redirect} from 'react-router-dom';
 import Login from '../Login/Login';
 import Registration from '../Registration/Registration';
 import RestorePass from '../RestorePass/RestorePass';
 import ChangePass from '../ChangePass/ChangePass';
-import {useDispatch, useSelector} from "react-redux";
-import {AppStateType} from "../../../redux/store";
-import {appActions, fetchProfileDataTC} from '../../../redux/appReducer/appReducer';
-import {UserResponeType} from '../../../redux/loginReducer/authReducer';
+import {useSelector} from "react-redux";
+import { AppStateType } from '../../../redux/store';
+
 
 export const Auth: React.FC = (props) => {
-    const dispatch = useDispatch()
+    // const dispatch = useDispatch()
     const isLoggedIn = useSelector<AppStateType, boolean>(state => state.authReducer.isLoggedIn)
-    const usersData = useSelector<AppStateType, UserResponeType[]>(state => state.authReducer.users)
-    useEffect(() => {
-        if (usersData.length === 0) {
-            dispatch(fetchProfileDataTC());
-        } else if (isLoggedIn) {
-            dispatch(appActions.setInitializedAC(true))
-        }
-    }, [usersData])
+
+
+    if(isLoggedIn) {
+        return <Redirect to={'/profile'}/>
+    }
+    // const userData = useSelector<AppStateType, serverUserType | null>(state => state.authReducer.user)
+
+    // useEffect(() => {
+    //
+    // }, [])
 
     return (
         <div className={styles.container}>
             <div className={styles.box}>
                 <div className={styles.inner}>
                     <Switch>
-                        <Route path={'/auth/login'} exact={true} render={(props) => <Login styles={styles}/>}/>
-                        <Route path={'/auth/registration'} exact={true}
+                        <Route path={'/auth/login'} render={(props) => <Login styles={styles}/>}/>
+                        <Route path={'/auth/registration'}
                                render={(props) => <Registration styles={styles}/>}/>
-                        <Route path={'/auth/restore-password'} exact={true}
+                        <Route path={'/auth/restore-password'}
                                render={(props) => <RestorePass styles={styles}/>}/>
-                        <Route path={'/auth/change-password/:token?'} exact={true}
+                        <Route path={'/auth/change-password/:token?'}
                                render={(props) => <ChangePass styles={styles}/>}/>
                     </Switch>
                 </div>
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,11 +1,12 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import authReducer from './loginReducer/authReducer';
+import authReducer from './authReducer/authReducer';
 import profileReducer from './profileReducer/profileReducer';
 import registrationReducer from './registrationReducer/registrationReducer';
 import restorePassReducer from './restorePassReducer/restorePassReducer';
 import changePassReducer from './changePassReducer/changePassReducer';
 import appReducer from "./appReducer/appReducer";
+import {composeWithDevTools} from "redux-devtools-extension";
 
 export const rootReducer = combineReducers({
     authReducer,
@@ -18,7 +19,10 @@ export const rootReducer = combineReducers({
 
 export type AppStateType = ReturnType<typeof rootReducer>
 
-export const store = createStore(rootReducer, applyMiddleware(thunk));
+// export const store = createStore(rootReducer, applyMiddleware(thunk));
 
-
+export const store = createStore(rootReducer, composeWithDevTools(
+    applyMiddleware(thunk),
+    // other store enhancers if any
+));
 
