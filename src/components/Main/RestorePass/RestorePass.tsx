2614def83e41807830d58b490a79477da8d20cd7
TheLevius <reflevius@gmail.com>
2021-06-07 13:17:08 +0300

    refactor: restorePass and ChangePass components

--- src/components/Main/RestorePass/RestorePass.tsx
+++ src/components/Main/RestorePass/RestorePass.tsx
@@ -1,21 +1,28 @@
-import React, {useState} from 'react';
+import React, {ChangeEvent, useCallback, useState} from 'react';
 import { RestoreMailTC} from "../../../redux/restorePassReducer/restorePassReducer";
 import {useDispatch, useSelector} from "react-redux";
 import {AppStateType} from "../../../redux/store";
 import {CheckEmail} from "../Registration/CheckEmail";
+import {Button, FormControl, FormHelperText, Input, InputLabel} from '@material-ui/core';
+import {NavLink} from 'react-router-dom';
 
 type PropsType = {
     styles: any
 }
 
 const RestorePass: React.FC<PropsType> = ({styles, ...props}) => {
-    const [emailIn, setEmailIn] = useState('collabincubator@gmail.com');
+    const [requestedEmail, setRequestedEmail] = useState('');
     const dispatch = useDispatch()
     const email = useSelector<AppStateType,boolean>(state => state.restorePassReducer.email)
 
     const onClickHandler = () => {
-        dispatch(RestoreMailTC(emailIn))
+        dispatch(RestoreMailTC(requestedEmail))
     }
+
+    const setEmailHandler = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+        setRequestedEmail(e.currentTarget.value);
+    }, [requestedEmail])
+
 
     return (
         <>
@@ -24,29 +31,31 @@ const RestorePass: React.FC<PropsType> = ({styles, ...props}) => {
                 <>
                     <h1>Cards</h1>
                     <h2>Forgot your password?</h2>
-                    <span>Email</span>
-                    <input className={styles.textInput}
-                           placeholder={'Email'}
-                           value={emailIn}
-                           onChange={(e) => {
-                               setEmailIn(e.currentTarget.value)
-                           }}
-                    />
-                    <div className={styles.describe}>
+                    <FormControl className={styles.controlInputs}>
+                        <InputLabel htmlFor={"requestedEmail"}>Enter your email</InputLabel>
+                        <Input id={"requestedEmail"}
+                               value={requestedEmail}
+                               onChange={setEmailHandler}
+                               aria-describedby={'requestEmail-error'}/>
+                    </FormControl>
+                    <p>
                         Enter your email address and we will send you further instructions
-                    </div>
-
-                    <button className={styles.btn}
+                    </p>
+                    <Button disabled={false} type={'submit'}
+                            className={styles.formButtons}
+                            variant={"contained"}
+                            color={"primary"}
                             onClick={onClickHandler}
-                    >Send Instructions
-                    </button>
+                    >
+                        Send Instructions
+                    </Button>
+                    <div className={styles.footerBox}>
+                        <p>Did you remember your password?</p>
+                        <NavLink to={'registration'} className={styles.footerLink}>
+                            <span>Sign Up</span>
+                        </NavLink>
+                    </div>
 
-                    <div className={styles.describe}>
-                        Did you remember your password?
-                    </div>
-                    <div>
-                        Try logging in
-                    </div>
 
                 </>}</>
     )
