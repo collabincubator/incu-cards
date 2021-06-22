6c081cf6a14ad2b2b3f821ebf3c7b91573581be8
valtikamore <77681004+valtikamore@users.noreply.github.com>
2021-06-22 18:55:38 +0300

    feat : table / popup cards

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Navigation from './Navigation/Navigation';
 import styles from './Header.module.css';
-import {FormControlLabel, FormGroup, Switch} from "@material-ui/core";
 
 const Header = (props: any) => {
     return <div className={styles.header}>
--- src/components/Main/Cards/Card/Card.tsx
+++ src/components/Main/Cards/Card/Card.tsx
@@ -1,19 +1,17 @@
 import React, {FC} from 'react'
 import {cardType} from "../../../../api/cards-api";
-import styles from "./Card.module.scss";
-
 
 export const Card:FC<cardType> = ({user_id,_id,created,updated,grade,type,rating,cardsPack_id,question,shots,answer,...props}) => {
 
     return (
-        <div className={styles.card}>
-            <div>
+        <tr >
+            <td>
                 {question}
-            </div>
-            <div>{answer}</div>
-            <div>{updated}</div>
-            <div>{grade.toFixed(2)}</div>
+            </td>
+            <td>{answer}</td>
+            <td>{updated}</td>
+            <td>{grade}</td>
 
-        </div>
+        </tr>
     )
 }
